de9fa357abaa867406a13c5298763339de8a99cc
Mark DeHate <mrdehate@gmail.com>
2014-03-04 18:32:35 -0500

    Exported from code pen
    first commit!

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/mrdehate/pen/hmjep.
+
+ For a given last.fm group, gets all the members, and displays cover art for whoever is listening right now. Uses a lot of API calls, because I can't figure out a better way to do it.
---
+++ js/index.js
@@ -0,0 +1,78 @@
+
+
+function getRecentArt(userName) {
+  var url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+userName+"&limit=2&api_key=e63ca8d5b65415a4ee36b32260dce956&format=json";
+
+  $.getJSON(url, function(data) {
+    var artist = data.recenttracks.track[0].artist["#text"];
+    var song = data.recenttracks.track[0]["name"];
+    var cover = data.recenttracks.track[0].image[3]["#text"];
+
+    if (data.recenttracks.track[0]["@attr"]["nowplaying"] = true) {
+
+      if (cover == "") {
+        cover = "http://upload.wikimedia.org/wikipedia/commons/d/d7/No_Cover_.jpg";
+      }
+      var htmlString = "";
+      htmlString = "<div class='entry'>";
+      htmlString += "<div class='user'>"+userName+"</div>";
+      htmlString += "<div class='artist'>"+artist+"</div>";
+      htmlString += "<div class='song'>"+song+"</div>";
+      htmlString += "<img class='imgArt' src='"+cover+"' /><br />";
+      htmlString += "</div>";
+      $('.cover').append(htmlString);
+      //  $('.output').html(JSON.stringify(data));
+    }
+
+  });
+
+  return 0;
+};
+
+function getGroupMembers() {
+  //   alert('hi there');
+  //  var groupMembers = new Array();
+  var theGroup = "350 groups";
+  var theGroup = "WorkForce Software";
+  $('.groupName').html(theGroup+" @ Last.fm");
+
+  var url = "http://ws.audioscrobbler.com/2.0/?method=group.getmembers&api_key=e63ca8d5b65415a4ee36b32260dce956&group="+theGroup+"&format=json";
+
+  $.getJSON(url, function(data) {
+   
+    var users = data.members.user;
+    var groupMembers = new Array();    
+
+    $('.cover').html("");
+    
+    var i=0;
+
+    while (data.members.user[i]) {
+      groupMembers[i] = data.members.user[i].name;
+      i++;
+    }
+
+    var j=0;
+    while (groupMembers[j]) {
+      j++;
+      var hi = getRecentArt(groupMembers[j]);
+    }
+
+    var time = new Date();
+    h = time.getHours(); 
+    m = time.getMinutes();
+    s = time.getSeconds();
+    $('.updateTime').html(h+":"+m+":"+s);
+    
+  });
+
+  return groupMembers;
+};
+
+
+$(document).ready(function() {
+
+  var members = getGroupMembers();
+//  setInterval(getGroupMembers, 10000);
+
+});
