ca9011a984a9d3b86e9f016be5399defa1b735bf
Mark DeHate <mrdehate@gmail.com>
2014-07-17 23:59:25 -0400

    Remaining bits
    Adding back in the artist charts; moving constants to an object, URL
    encoding

--- js/oo-aa.js
+++ js/oo-aa.js
@@ -1,21 +1,20 @@
-//Global Variables yay!
-var LASTFM_API_KEY = "e63ca8d5b65415a4ee36b32260dce956";
-var ECHONEST_API_KEY = "TU0XNU2PKBGD5AIJ2";
-
- // var THE_GROUP = "350 groups";
- // var THE_GROUP = "The Musical Elitists";
-var THE_GROUP = "WorkForce Software";
-var BLANK_COVER_URL = "http://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
-var REFRESH_INTERVAL = 60000;
-var DAY_REFRESH_INTERVAL = 86400000;
 
 var gApiCallCounter = 0; 
 var gStartupTime = Date.now();
 
-//@TODO: temp for debugging
-var groupMembers = new Array();
+function Constants() {
+	this.LASTFM_API_KEY = "e63ca8d5b65415a4ee36b32260dce956";
+	this.ECHONEST_API_KEY = "TU0XNU2PKBGD5AIJ2";
 
-function Listen(inUserName) {
+	 // var THE_GROUP = "350 groups";
+	 // var THE_GROUP = "The Musical Elitists";
+	this.THE_GROUP = "WorkForce Software";
+	this.BLANK_COVER_URL = "http://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg";
+	this.REFRESH_INTERVAL = 60000;
+	this.DAY_REFRESH_INTERVAL = 86400000;
+}
+
+function Listen(inUserName, inConstants) {
 	//Public Variables
 	this.userName = inUserName;
 	this.artist = "";
@@ -28,8 +27,8 @@ function Listen(inUserName) {
 	this.apiCount = 0;
 
 	//Private Variables
-//	var url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+this.userName+"&limit=2&api_key="+LASTFM_API_KEY+"&format=json";
 	var self = this;
+	var constants = inConstants;
 
 	/**
 	*
@@ -51,7 +50,7 @@ function Listen(inUserName) {
 	        tempArt = getArtistArt();
 
 	        if (tempArt === "") {
-	        	tempArt = BLANK_COVER_URL;
+	        	tempArt = constants.BLANK_COVER_URL;
 	        }
 	      }
 	    }
@@ -71,7 +70,7 @@ function Listen(inUserName) {
 	  var tempArt = "";
 
 	  $.ajax({
-	    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+self.artist+"&api_key="+LASTFM_API_KEY+"&format=json",
+	    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="+encodeURIComponent(self.artist)+"&api_key="+constants.LASTFM_API_KEY+"&format=json",
 	    async: false
 	  }).done(function( a1 ) {
 	    try{
@@ -129,7 +128,7 @@ function Listen(inUserName) {
 	var tempArt = "";
 
 	  $.ajax({
-	    url: "http://developer.echonest.com/api/v4/song/search?api_key=" + ECHONEST_API_KEY + "&format=json&results=1&artist=" + self.artist + "&title=" + self.song + "&bucket=id:7digital-US&bucket=tracks",
+	    url: "http://developer.echonest.com/api/v4/song/search?api_key=" + constants.ECHONEST_API_KEY + "&format=json&results=1&artist=" + encodeURIComponent(self.artist) + "&title=" + encodeURIComponent(self.song) + "&bucket=id:7digital-US&bucket=tracks",
 	    async: false
 	  }).done(function( a1 ) {
 	    try{
@@ -146,7 +145,7 @@ function Listen(inUserName) {
 
 	//Begin
 	  $.ajax({
-	    url: "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+this.userName+"&limit=2&api_key="+LASTFM_API_KEY+"&format=json",
+	    url: "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+this.userName+"&limit=2&api_key="+constants.LASTFM_API_KEY+"&format=json",
 	    async: false
 	  }).done(function( data ) {
 
@@ -171,8 +170,6 @@ function Listen(inUserName) {
 		} catch (meh) {
 			self.valid = false;
 		}
-//console.log(data);
-//		console.log("self:"+ self.getAlbumArt(""));
 	});
 
 //@TODO: Would be cool if we had a "debug" switch at the top of the file
@@ -199,9 +196,88 @@ function processListen(inListen, inTargetClass) {
 }
 
 
-function main() {
+/**
+*
+* Displays chart of the top artists
+* 
+*/
+function getArtistChart() {
+//  $('.groupName').html(theGroup+" @ Last.fm");
 
-	  var url = "http://ws.audioscrobbler.com/2.0/?method=group.getmembers&api_key="+LASTFM_API_KEY+"&group="+THE_GROUP+"&format=json";
+  var constants = new Constants();
+
+  var url = "http://ws.audioscrobbler.com/2.0/?method=group.getweeklyartistchart&api_key="+constants.LASTFM_API_KEY+"&group="+constants.THE_GROUP+"&format=json";
+
+  $.getJSON(url, function(data) {
+   
+//    $('.output').append(JSON.stringify(data) + "<br /><br />");
+
+//*** handle the chart dates ***
+    var chartStart = new Date();
+    var chartEnd = new Date();
+
+    try{
+      chartStart.setTime(data.weeklyartistchart["@attr"].from*1000);   //last.fm sends this value in seconds since 1970; JS data object uses milliseconds
+      chartEnd.setTime(data.weeklyartistchart["@attr"].to*1000);
+    }
+    catch (meh) {
+      chartStart = null;
+      chartEnd = null;
+    }
+
+    $('.artistChart h3').html("");
+    if (chartStart && chartEnd) {
+      $('.artistChart h3').html($.datepicker.formatDate('M dd', chartStart) + " - " + $.datepicker.formatDate('M dd', chartEnd));
+    }
+
+//*** get the artists ***
+    var topArtists = new Array();
+    var i=0;
+
+    while (data.weeklyartistchart.artist[i]) {
+//      console.log(data.weeklyartistchart.artist[i].playcount);
+      if(data.weeklyartistchart.artist[i].playcount && data.weeklyartistchart.artist[i].playcount > 1) {
+        topArtists[i] = data.weeklyartistchart.artist[i].name;
+      } else {
+        break;
+      }
+      i++;
+    }
+
+    $('.artistChart ol').html("");
+
+    var j;
+
+    for (j = 0; j < 25 ; j++) { 
+      if (topArtists[j] === undefined) {
+        break;
+      }
+
+      $('.artistChart ol').append("<li>"+topArtists[j]+"</li>");
+    }
+
+    gApiCallCounter++;
+
+  });
+
+  return true;
+}
+
+
+/**
+*
+* Update the api counter to list API calls per second
+* 
+*/
+function updateApiCounter() {
+  $(".stats").html("API/sec: " + (gApiCallCounter/((Date.now() - gStartupTime)/1000)).toFixed(2));
+}
+
+
+function main() {
+	var constants = new Constants();
+	var groupMembers = new Array();
+	var url = "http://ws.audioscrobbler.com/2.0/?method=group.getmembers&api_key="+constants.LASTFM_API_KEY+"&group="+constants.THE_GROUP+"&format=json";
 
   $.getJSON(url, function(data) {
    
@@ -211,7 +287,7 @@ function main() {
     var i=0;
 
     while (data.members.user[i]) {
-      groupMembers[i] = new Listen(data.members.user[i].name);
+      groupMembers[i] = new Listen(data.members.user[i].name, constants);
       i++;
     }
 
@@ -233,4 +309,9 @@ function main() {
   return true;
 }
 
-setInterval(main, REFRESH_INTERVAL);
+setInterval(main, (new Constants()).REFRESH_INTERVAL);
+setInterval(updateApiCounter, (new Constants()).REFRESH_INTERVAL);
+
+getArtistChart();
+setInterval(getArtistChart, (new Constants()).DAY_REFRESH_INTERVAL);
+
