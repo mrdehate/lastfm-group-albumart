c69c1c25ef5a8957c0dea10576de5c7ec95d7f02
Mark DeHate <mrdehate@gmail.com>
2014-09-26 17:00:16 -0400

    Traffic Map update
    Changing how the traffic map updating works

--- js/traffic_map.js
+++ js/traffic_map.js
@@ -1,15 +1,33 @@
+function updateTrafficOnMap(map, trafficLayer, on)
+{
+  if(on === 0) {
+    trafficLayer.setMap(null);
+    trafficLayer = null;
+    setTimeout(function() { updateTrafficOnMap(map, null, 1); }, 1);
+  }
+  
+  if(on === 1) {
+    var trafficLayer2 = new google.maps.TrafficLayer();
+    trafficLayer2.setMap(map);
+    // after 60 seconds. update the traffic map
+    setTimeout(function() { updateTrafficOnMap(map, trafficLayer2, 0);}, 60000);
+  }
+}
+
 function initialize() {
   var myLatlng = new google.maps.LatLng(42.45887, -83.21938);
   var mapOptions = {
     zoom: 9,
-    center: myLatlng
+    center: myLatlng,
+    disableDefaultUI: true
   };
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  var trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
+/*  var trafficLayer = new google.maps.TrafficLayer();
+  trafficLayer.setMap(map); */
+  updateTrafficOnMap(map, null, 1);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
-setInterval(initialize, 60000);
+//setInterval(initialize, 60000);
