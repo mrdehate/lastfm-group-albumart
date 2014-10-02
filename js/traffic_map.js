1c5624e0bf5134cff0be1547db2489444c9a7667
Mark DeHate <mrdehate@gmail.com>
2014-10-02 11:36:26 -0400

    Updating GPS coordinates
    Updating GPS to center on Livonia, also getting rid of some comments

--- js/traffic_map.js
+++ js/traffic_map.js
@@ -1,40 +1,5 @@
-/*function updateTrafficOnMap(map, trafficLayer, on)
-{
-    console.log("TL IN");
-    console.log(trafficLayer);
-    console.log(map);
-
-  if(on === 0) {
-    trafficLayer.setMap(null);
-    trafficLayer = null;
-    setTimeout(function() { updateTrafficOnMap(map, null, 1); }, 2000);
-  }
-  
-  if(on === 1) {
-    var trafficLayer2 = new google.maps.TrafficLayer();
-    trafficLayer2.setMap(map);
-    console.log("TL2");
-    console.log(trafficLayer2);
-    // after 60 seconds. update the traffic map
-    setTimeout(function() { updateTrafficOnMap(map, trafficLayer2, 0);}, 6000);
-  }
-
-}
-*/
-
-/*function updateTrafficOnMap(map, trafficLayer)
-{
-    trafficLayer.setMap(null);
-    trafficLayer = null;
-    var trafficLayer2 = new google.maps.TrafficLayer();
-    trafficLayer2.setMap(map);
-    // after 60 seconds. update the traffic map
-    setTimeout(function() { updateTrafficOnMap(map, trafficLayer2);}, 6000);
-}
-*/
-
 function initialize() {
-  var myLatlng = new google.maps.LatLng(42.45887, -83.21938);
+  var myLatlng = new google.maps.LatLng(42.4236417,-83.4228403);
   var mapOptions = {
     zoom: 9,
     center: myLatlng,
@@ -45,7 +10,6 @@ function initialize() {
 
   var trafficLayer = new google.maps.TrafficLayer();
   trafficLayer.setMap(map);
-//  updateTrafficOnMap(map, null, 1);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
